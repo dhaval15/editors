2b61cd662e0aaf92a9b06dcd22fb5be504116962
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-09-09 18:53:38 +0000

    improvement: endpoints now starts with /api

--- api/index.js
+++ api/index.js
@@ -8,19 +8,19 @@ app.use(cors())
 
 const draftController = require('./controllers/draftController');
 
-app.get('/drafts', draftController.listDrafts);
-app.get('/draft/:id', draftController.getDraft);
-app.put('/draft/:id', draftController.updateDraft);
-app.delete('/draft/:id', draftController.deleteDraft);
-app.post('/draft', draftController.createDraft);
+app.get('/api/drafts', draftController.listDrafts);
+app.get('/api/draft/:id', draftController.getDraft);
+app.put('/api/draft/:id', draftController.updateDraft);
+app.delete('/api/draft/:id', draftController.deleteDraft);
+app.post('/api/draft', draftController.createDraft);
 
 const sceneController = require('./controllers/sceneController');
 
-app.get('/draft/:id/scenes', sceneController.listScenes);
-app.get('/draft/:id/scene/:sceneId', sceneController.getScene);
-app.put('/draft/:id/scene/:sceneId', sceneController.updateScene);
-app.delete('/draft/:id/scene/:sceneId', sceneController.deleteScene);
-app.post('/draft/:id/scenes', sceneController.createScene);
+app.get('/api/draft/:id/scenes', sceneController.listScenes);
+app.get('/api/draft/:id/scene/:sceneId', sceneController.getScene);
+app.put('/api/draft/:id/scene/:sceneId', sceneController.updateScene);
+app.delete('/api/draft/:id/scene/:sceneId', sceneController.deleteScene);
+app.post('/api/draft/:id/scenes', sceneController.createScene);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
